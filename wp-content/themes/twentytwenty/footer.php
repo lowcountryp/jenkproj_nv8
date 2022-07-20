4c96c108eb6a2bec87eeef422f9ea6b20b1e091f
lowcountryp <philmarnell@gmail.com>
2022-07-20 17:10:45 -0400

    Chg powered to powDered in footer

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+								<?php _e( 'Powdered by WordPress', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
